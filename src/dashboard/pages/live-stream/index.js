81531fa605d2a588ed54d0fe87d00268492452c9
umardar8 <umerfarooqdar.official@gmail.com>
2024-06-06 01:37:58 +0500

    Dashboard cards, streams table, and users tables with modals in superadmin and admin

--- src/dashboard/pages/live-stream/index.js
+++ src/dashboard/pages/live-stream/index.js
@@ -1,6 +1,462 @@
-import React from "react";
+import React, { useState } from "react";
+import { Stack, Textarea } from "@chakra-ui/react";
+import CustomTable from "../../../components/table";
+import BaseColors from "../../../constant";
+import PrimaryButton from "../../../components/button";
+import InputField from "../../../components/input-field";
+import { Styles } from "../../../styles";
+import PrimaryModal from "../../../components/modal";
+import { IoFunnelOutline } from "react-icons/io5";
+import { IoMdMore, IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { BiVideoPlus } from "react-icons/bi";
+import { MdEditNote, MdOutlineDisabledVisible } from "react-icons/md";
+import { TiTickOutline } from "react-icons/ti";
+import { FaRegCheckCircle } from "react-icons/fa";
+import { CiCircleCheck } from "react-icons/ci";
+
 // import StreamingForm from "../../../components/streaming-form";
 
+const inputStyle = {
+  borderRadius: "2px",
+  height: "5vh",
+  width: "100%",
+  padding: "15px 20px",
+  color: BaseColors.black,
+  border: `1px solid ${BaseColors.light_grey},`,
+};
+
 export default function LiveStream() {
-  return <div>abc</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMoreModalOpen, setIsMoreModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const showMoreModal = () => {
+    setIsMoreModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const handleMoreCancel = () => {
+    setIsMoreModalOpen(false);
+  };
+
+  let data = [
+    {
+
+      streamId: 1,
+      id: 1,
+      Title: "Foreign Scholarships",
+      Description: "Students' Awareness Live Session",
+      Category: "Education",
+      Edit: 
+      <PrimaryButton 
+        leftIcon={ <MdEditNote style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      Disable:
+      <PrimaryButton 
+        leftIcon={ <MdOutlineDisabledVisible style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      More: 
+      <PrimaryButton 
+        leftIcon={ <IoMdMore style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+        onClick={showMoreModal}
+      />,
+    },
+    {
+      streamId: 2,
+      id: 2,
+      Title: "Heat Stroke in MENA Region",
+      Description: "Report on Global Warming",
+      Category: "Climate",
+      Edit: 
+      <PrimaryButton 
+        leftIcon={ <MdEditNote style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      Disable:
+      <PrimaryButton 
+        leftIcon={ <MdOutlineDisabledVisible style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      More: 
+      <PrimaryButton 
+        leftIcon={ <IoMdMore style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+        onClick={showMoreModal}
+      />,
+    },
+    {
+      streamId: 3,
+      id: 3,
+      Title: "Live Job Fair UAE",
+      Description: "Live opportunities session",
+      Category: "Career",
+      Edit: 
+      <PrimaryButton 
+        leftIcon={ <MdEditNote style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      Disable:
+      <PrimaryButton 
+        leftIcon={ <MdOutlineDisabledVisible style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      More: 
+      <PrimaryButton 
+        leftIcon={ <IoMdMore style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+        onClick={showMoreModal}
+      />,
+    },
+    {
+      streamId: 4,
+      id: 4,
+      Title: "Analysis of Pandemics",
+      Description: "Safeguard yourself during Pandemics",
+      Category: "Health",
+      Edit: 
+      <PrimaryButton 
+        leftIcon={ <MdEditNote style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      Disable:
+      <PrimaryButton 
+        leftIcon={ <MdOutlineDisabledVisible style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      More: 
+      <PrimaryButton 
+        leftIcon={ <IoMdMore style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+        onClick={showMoreModal}
+      />,
+    },
+    {
+      streamId: 5,
+      id: 5,
+      Title: "Nanotechnology Advancements",
+      Description: "The future of Technologies",
+      Category: "Technology",
+      Edit: 
+      <PrimaryButton 
+        leftIcon={ <MdEditNote style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      Disable:
+      <PrimaryButton 
+        leftIcon={ <MdOutlineDisabledVisible style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      More: 
+      <PrimaryButton 
+        leftIcon={ <IoMdMore style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+        onClick={showMoreModal}
+      />,
+    },
+    {
+      streamId: 6,
+      id: 6,
+      Title: "The Rise of Startup",
+      Description: "How to start your business",
+      Category: "Career",
+      Edit: 
+      <PrimaryButton 
+        leftIcon={ <MdEditNote style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      Disable:
+      <PrimaryButton 
+        leftIcon={ <MdOutlineDisabledVisible style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      More: 
+      <PrimaryButton 
+        leftIcon={ <IoMdMore style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+        onClick={showMoreModal}
+      />,
+    },
+    {
+      streamId: 7,
+      id: 7,
+      Title: "The future of Medication",
+      Description: "Latest advancements in Medicine",
+      Category: "Health",
+      Edit: 
+      <PrimaryButton 
+        leftIcon={ <MdEditNote style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      Disable:
+      <PrimaryButton 
+        leftIcon={ <MdOutlineDisabledVisible style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      More: 
+      <PrimaryButton 
+        leftIcon={ <IoMdMore style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+        onClick={showMoreModal}
+      />,
+    },
+    {
+      streamId: 8,
+      id: 8,
+      Title: "Political Issues of World",
+      Description: "No Description",
+      Category: "Career",
+      Edit: 
+      <PrimaryButton 
+        leftIcon={ <MdEditNote style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      Disable:
+      <PrimaryButton 
+        leftIcon={ <IoMdCheckmarkCircleOutline style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+      />,
+      More: 
+      <PrimaryButton 
+        leftIcon={ <IoMdMore style={{ fontSize: "1.5em" }} /> }
+        btnStyle={{ background: "transparent", padding: 0, margin: 0 }}
+        onClick={showMoreModal}
+      />,
+    },
+  ];
+
+  return (
+    <>
+      <div className="">
+        <div className="row p-0 m-0">
+          {/* start header */}
+          <div className="col-12 mb-2 ">
+            <div className="row d-flex justify-content-between align-items-center my-2">
+              <div className="col-6 col-lg-4 d-flex flex-column justify-content-center">
+                <h3 className="text-dark text-family mt-2">Streams Detail</h3>
+              </div>
+              <div className="col-6 col-lg-4  d-flex flex-column justify-content-center">
+                <div className="d-flex justify-content-end ">
+                <PrimaryButton
+                    leftIcon={
+                      <IoFunnelOutline
+                        style={{
+                          fontSize: "1.5em",
+                          color: "rgb(148, 148, 148)",
+                        }}
+                      />
+                    }
+                    btnStyle={{ background: "transparent", marginRight: "10px" }}
+                    btnClassName="border-1 rounded-1"
+                    onClick={showModal}
+                  />
+                  <PrimaryButton
+                    leftIcon={
+                      <BiVideoPlus
+                        style={{
+                          fontSize: "1.5em",
+                          color: "rgb(148, 148, 148)",
+                        }}
+                      />
+                    }
+                    btnStyle={{ background: "transparent" }}
+                    btnClassName="border-1 rounded-1"
+                    onClick={showModal}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* end  header */}
+
+          <div className="col-12">
+            <CustomTable
+              cols={[
+                {
+                  heading: "Id",
+                  key: "id",
+                },
+                {
+                  heading: "Title",
+                  key: "Title",
+                },
+                {
+                  heading: "Description",
+                  key: "Description",
+                },
+                {
+                  heading: "Category",
+                  key: "Category",
+                },
+                {
+                  heading: "Edit",
+                  key: "Edit",
+                },
+                {
+                  heading: "Disable",
+                  key: "Disable",
+                },
+                {
+                  heading: "More",
+                  key: "More",
+                },
+              ]}
+              datasourse={data}
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* model foam */}
+      <PrimaryModal
+        open={isModalOpen}
+        onCancel={handleCancel}
+        styles={{
+          display: "flex",
+          width: "40vw",
+          justifyContent: "center",
+          alignItems: "center",
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          transform: "translate(-50%, -50%)",
+        }}
+        Modalbody={
+          <>
+            <div className="row container p-4 d-flex">
+              <h2 className="text-secondary justify-content-center d-flex">
+                Stream Form
+              </h2>
+              <div className="col-12 align-items-center text-black px-4">
+                <Stack spacing={2} className="text-black">
+                  <InputField
+                    isBlack={true}
+                    label="Title"
+                    type="text"
+                    placeholder="enter stream title"
+                    inputStyle={inputStyle}
+                  />
+                  <div style={{ marginLeft: "10px" }}>
+                    <p
+                      className="my-1 text-secondary"
+                      style={Styles.headerItem3}
+                    >
+                      Description
+                    </p>
+                  <Textarea 
+                    placeholder='enter description of stream' 
+                    style={{
+                      borderRadius: "2px",
+                      width: "97%",
+                      padding: "15px 20px",
+                      color: BaseColors.black,
+                      border: `1px solid ${BaseColors.light_grey},`,
+                    }}
+                    />
+                  </div>
+                  <InputField
+                    isBlack={true}
+                    label="RTMP URL"
+                    placeholder="enter link to the stream"
+                    inputStyle={inputStyle}
+                  />
+                  <InputField
+                    isBlack={true}
+                    label="RTMP Key"
+                    placeholder="enter link to the stream"
+                    inputStyle={inputStyle}
+                  />
+                  <div style={{ marginLeft: "10px" }}>
+                    <p
+                      className="my-1 text-secondary"
+                      style={Styles.headerItem3}
+                    >
+                      Category{" "}
+                    </p>
+                    <select
+                      className="px-2"
+                      style={{
+                        border: `1px solid ${BaseColors.light_grey}`,
+                        borderRadius: "2px",
+                        color: BaseColors.light_grey,
+                        height: "5vh",
+                        width: "97%",
+                        marginBottom: "20px"
+                      }}
+                    >
+                      <option value="0">select</option>
+                      <option value="1">Health</option>
+                      <option value="2">Education</option>
+                      <option value="3">technology</option>
+                      <option value="4">Climate</option>
+                      <option value="5">Career</option>
+                    </select>
+                  </div>
+                </Stack>
+                <div className="col d-flex justify-content-end mt-2">
+                  <PrimaryButton
+                    label="Add"
+                    btnClassName="me-2"
+                    btnStyle={{ borderRadius: "2px", marginEnd: "10px" }}
+                    colorScheme="purple"
+                  />
+                  <PrimaryButton
+                    label="Cancel"
+                    variant="outline"
+                    btnStyle={{
+                      borderRadius: "2px",
+                      background: "transparent",
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </>
+        }
+      />
+
+      {/* more info modal */}
+      <PrimaryModal
+        open={isMoreModalOpen}
+        onCancel={handleMoreCancel}
+        styles={{
+          display: "flex",
+          width: "40vw",
+          justifyContent: "center",
+          alignItems: "center",
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          transform: "translate(-50%, -50%)",
+        }}
+        Modalbody={
+          <>
+            <div className="row container p-4 d-flex">
+              <h2 className="text-secondary justify-content-center d-flex">
+                More Information
+              </h2>
+              <div className="col-12 align-items-center text-black px-4">
+                  <p className="heading-3 mb-2">Uploaded by</p>
+                  <p className="heading-2 mb-1"><b>Name:</b> name_of_user</p>
+                  <p className="heading-2 mb-1"><b>Username:</b> username_of_user</p>
+                  <p className="heading-2"><b>Email:</b> email_of_user@email.com</p>
+                  <p className="heading-3 mb-2">Streamed On</p>
+                  <p className="heading-2 mb-1"><b>Date:</b> dd_mm_yyyy</p>
+                  <p className="heading-2 mb-1"><b>Time:</b> hh:mm</p>
+                  <p className="heading-2"><b>Duration:</b> 02 hour 25 min</p>
+              </div>
+            </div>
+          </>
+        }
+      />
+    </>
+  )
 }
--- src/dashboard/pages/super-admin-dashboard/main-dashboard/index.js
+++ src/dashboard/pages/super-admin-dashboard/main-dashboard/index.js
@@ -1,7 +1,33 @@
 import React from "react";
+import PrimaryButton from '../../../../components/button';
+import { MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
+import { IoMdMore } from "react-icons/io";
+import { HiOutlineArrowNarrowUp, HiOutlineArrowNarrowDown } from "react-icons/hi";
+
+const cardClassName = "col-sm-3 d-flex row rounded-1 shadow bg-white p-4 justify-content-center";
+const contentClassName = "d-flex justify-content-center pb-4";
 
 function MainDashboard() {
-  return <div>MainDashboard</div>;
+  return <>
+  <div className="row m-4 d-flex justify-content-around">
+    <div className={cardClassName}>
+      <p>Activities</p>
+      <h2 className={contentClassName}>2463 <HiOutlineArrowNarrowUp style={{color: "green", fontSize: "1.1em"}} /></h2>
+      <PrimaryButton label='show details' background="black" rightIcon={<IoMdMore />} />
+    </div>
+    <div className={cardClassName}>
+      <p>Users</p>
+      <h2 className={contentClassName}>1525 <HiOutlineArrowNarrowUp style={{color: "green", fontSize: "1.1em"}} /></h2>
+      <PrimaryButton label='show details' background="black" rightIcon={<IoMdMore />} />
+    </div>
+    <div className={cardClassName}>
+      <p>Streams</p>
+      <h2 className={contentClassName}>986 <HiOutlineArrowNarrowDown style={{color: "red", fontSize: "1.1em"}} /></h2>
+      <PrimaryButton label='show details' background="black" rightIcon={<IoMdMore />} />
+    </div>
+    
+  </div>;
+  </>
 }
 
 export default MainDashboard;
